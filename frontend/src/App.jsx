05a3381eb25d11ad907c9493eae07c679ebb8cd1
ElyasBelkhir <belkhirelyas@gmail.com>
2023-11-17 20:48:36 -0600

    firebase setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBPFPRCkDm0lbeLQOvym-bp0K9pI8JJI5Y",
+  authDomain: "aim-project-cd9e0.firebaseapp.com",
+  projectId: "aim-project-cd9e0",
+  storageBucket: "aim-project-cd9e0.appspot.com",
+  messagingSenderId: "644788453808",
+  appId: "1:644788453808:web:e35c4695086b51bc6e5675",
+  measurementId: "G-ECBERP0P8P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
